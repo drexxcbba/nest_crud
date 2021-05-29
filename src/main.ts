646aea7c0e5d9c9aaccf27887fe1a223c9982739
unknown <villarroel24kyle@gmail.com>
2021-05-29 13:11:15 -0400

    ft: added validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   const logger = new Logger();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   logger.log(`Server is running in ${ await app.getUrl() }`);
 }
 bootstrap();
