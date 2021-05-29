fcfaf83dd94af427c7dce20b8b03a9686161f823
unknown <villarroel24kyle@gmail.com>
2021-05-29 14:08:42 -0400

    added docker and typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'myblog_user',
+      password: 'myblog_password',
+      database: 'myblog_db',
+      entities: [__dirname + './**/**/*entity{.ts, .js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+    }), 
+    PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
